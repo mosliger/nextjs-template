c7e4d148fcb75451820b2ed8e5469de72f0520c3
mosliger <chonlatee200436@gmail.com>
2019-09-23 14:53:14 +0700

    use react styleguidedist

---
+++ styleguide.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+const getComponentName = componentPath => {
+  const dir = path.dirname(componentPath);
+  const arrDir = dir.split('/');
+  const name = arrDir[arrDir.length - 1];
+  return name;
+};
+
+module.exports = {
+  getComponentPathLine(componentPath) {
+    const name = getComponentName(componentPath);
+    if (name === 'components') return '';
+    return `import ${name} from '../components/${name}'`;
+  },
+  ignore: [
+    'components/**/*style.js',
+    'components/Navigation/*'
+  ],
+  sections: [
+    {
+      name: 'UI Components',
+      components: 'components/**/*.js'
+    }
+  ],
+  webpackConfig: {
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        }
+      ]
+    }
+  }
+};
